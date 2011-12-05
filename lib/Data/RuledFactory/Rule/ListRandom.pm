5de329e8dc7971d5a31bac1d5fcabb2c3b21b7c7
Toru Yamaguchi <yamaguchi.toru@51547-mac.dena.local>
2011-12-05 20:12:29 +0900

    added

---
+++ lib/Data/RuledFactory/Rule/ListRandom.pm
@@ -0,0 +1,78 @@
+package Data::RuledFactory::Rule::ListRandom;
+
+use strict;
+use warnings;
+use Data::WeightedRoundRobin;
+
+our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    my $args = ref $_[0] ? $_[0] : { @_ };
+
+    %$args = (
+        list    => [],
+        next_cb => undef,
+        rows    => undef,
+        cursor  => 0,
+        %$args,
+    );
+
+    $args->{list} = Data::WeightedRoundRobin->new($args->{list});
+
+    bless $args => $class;
+}
+
+sub has_next {
+    my $self = shift;
+
+    unless ( defined $self->{rows} ) {
+        return 1;
+    }
+
+    return $self->{cursor} < $self->{rows} ? 1 : 0;
+}
+
+sub _next {
+    my $self = shift;
+    $self->{list}->next;
+}
+
+
+1;
+__END__
+
+=head1 NAME
+
+Data::RuledFactory::Rule::ListRandom -
+
+=head1 SYNOPSIS
+
+  use Data::RuledFactory::Rule::ListRandom;
+
+=head1 DESCRIPTION
+
+Data::RuledFactory::Rule::ListRandom is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# Local Variables:
+# mode: perl
+# perl-indent-level: 4
+# indent-tabs-mode: nil
+# coding: utf-8-unix
+# End:
+#
+# vim: expandtab shiftwidth=4:
+

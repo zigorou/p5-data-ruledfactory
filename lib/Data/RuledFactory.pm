2aa44e0abc97e7c6b290bfae3e0d792312da48dc
Toru Yamaguchi <yamaguchi.toru@51547-mac.dena.local>
2011-12-05 18:42:24 +0900

    initial commit

---
+++ lib/Data/RuledFactory.pm
@@ -0,0 +1,32 @@
+package Data::RuledFactory;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Data::RuledFactory -
+
+=head1 SYNOPSIS
+
+  use Data::RuledFactory;
+
+=head1 DESCRIPTION
+
+Data::RuledFactory is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
